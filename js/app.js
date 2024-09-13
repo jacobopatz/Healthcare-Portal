3f7a13b75e1f3f495dd7c89a49671698dcc0d6e4
Jacob-Opatz <126533622+Jacob-Opatz@users.noreply.github.com>
2024-09-12 18:30:47 -0700

    deleted

--- js/app.js
+++
@@ -1,9 +0,0 @@
-const button = document.getElementById('myb');
-// Add a click event listener to the button
-button.addEventListener('click', function() {
-    // Perform an action when the button is clicked
-    const username = document.getElementById('usr').value;
-    const pwd = document.getElementById('pwd').value;
-    alert(username + pwd);
-
-});
