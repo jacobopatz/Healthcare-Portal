d3d24c8cdb6a3e75d652cc2b4b72d14567437028
Jacob-Opatz <126533622+Jacob-Opatz@users.noreply.github.com>
2024-09-16 15:15:58 -0700

    Added log out button to all pages

--- js/template.js
+++ js/template.js
@@ -1,3 +1,6 @@
+function handleButtonClick(event) {
+    window.location.href = event.target.id + '.html';
+}
 document.getElementById('menu-toggle').addEventListener('click', function() {
     var menu = document.getElementById('menu');
     menu.classList.toggle('menu-open');
@@ -6,3 +9,4 @@ document.getElementById('menu-exit').addEventListener('click', function() {
     var menu = document.getElementById('menu');
     menu.classList.toggle('menu-open');
 });
+document.getElementById("index").addEventListener('click',handleButtonClick);
