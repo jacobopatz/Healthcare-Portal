b52800a6c8005064824fb92d75e8958826840dab
JVue4073 <114715871+JVue4073@users.noreply.github.com>
2024-11-24 16:54:34 -0800

    Pharmacy WIP
    It's in wip. Will fix the add/view prescriptions webpage to store info and display info properly and make some edits to allow removal of medication info row and generating a PDF document of patient information.

---
+++ pharmacy/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from .models import Medication
+from .models import Prescription
+
+class MedicationForm(forms.ModelForm):
+    class Meta:
+        model = Medication
+        fields = ['medication_name', 'dosage', 'frequency', 'start_date', 'end_date', 'prescribing_doctor']
+        widgets = {
+            'start_date': forms.DateInput(attrs={'type': 'date'}),
+            'end_date': forms.DateInput(attrs={'type': 'date'}),
+        }
+
+class PrescriptionForm(forms.ModelForm):
+    class Meta:
+        model = Prescription
+        fields = ['first_name', 'last_name', 'patient_id', 'age', 
+                  'medication', 'dosage', 'frequency', 
+                  'date_signed', 'signature']
+        widgets = {
+            'medication': forms.Textarea(attrs={'rows': 3, 'placeholder': 'Enter medications'}),
+            'dosage': forms.Textarea(attrs={'rows': 3, 'placeholder': 'Enter dosages'}),
+            'frequency': forms.Textarea(attrs={'rows': 3, 'placeholder': 'Enter frequencies'}),
+            'date_signed': forms.DateInput(attrs={'type': 'date'}),
+        }
