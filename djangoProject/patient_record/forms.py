f3bcc6cf81aede3f4bfd5039a22e966817d0c1c9
Jwell459 <69662801+Jwell459@users.noreply.github.com>
2024-11-25 12:31:33 -0800

    Added encounter record page
    Code is stored under the patientRecord directory

--- djangoProject/patient_record/forms.py
+++ djangoProject/patient_record/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from sharedModels.models import PatientRecord, Employees, Insurance
+from sharedModels.models import PatientRecord, Encounters, Employees, Insurance, LabOrders, LabTests, PharmacyOrder
 
 
 class PatientAddForm(forms.ModelForm):
@@ -63,5 +63,53 @@ class PatientEditForm(forms.ModelForm):
     changed_time = -1
 
 
+class EncounterAddForm(forms.ModelForm):
+    date = forms.DateTimeField(label='Date')
+    physicianid = forms.ModelChoiceField(
+        label="Physician",
+        queryset=Employees.objects.all()
+    )
+    patientid = forms.ModelChoiceField(
+        label="Patient",
+        queryset=PatientRecord.objects.all()
+    )
+    patientcomplaints = forms.CharField(label='Patient Complaints', max_length=120, required=False)
+    vitalsigns = forms.CharField(label='Vital Signs', max_length=120, required=False)
+    practitionernotes = forms.CharField(label='PractitionerNotes', max_length=120, required=False)
+    laborderid = forms.ModelChoiceField(
+        label="Lab Order",
+        queryset=LabOrders.objects.all(),
+        required=False
+    )
+    pharmacyorderid = forms.ModelChoiceField(
+        label="Pharmacy Order",
+        queryset=LabOrders.objects.all(),
+        required=False
+    )
+    treatment_plan = forms.CharField(label='Treatment Plan', max_length=120, required=False)
+    refferals = forms.CharField(label='Referrals', max_length=120, required=False)
+    recfollowup = forms.DateField(label='RecFollowUp', required=False)
+
+    class Meta:
+        model = Encounters
+        fields = ['date', 'physicianid', 'patientid', 'patientcomplaints', 'practitionernotes', 'vitalsigns', 'laborderid', 'pharmacyorderid', 'treatment_plan', 'refferals', 'recfollowup' ]
+
+class EncounterSearchForm(forms.Form):
+    search_query = forms.CharField(
+        label="Search for an encounter",
+        max_length=100,
+        required=False,
+        widget=forms.DateInput(attrs={'placeholder': 'Search by date'})
+    )
+    selected_encounter = forms.ModelChoiceField(
+        label="Selected Encounter",
+        queryset=Encounters.objects.none(),
+        required=False
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Initially, set the queryset for the 'patient' field to all patients
+        self.fields['selected_encounter'].queryset = Encounters.objects.all()
 
 
