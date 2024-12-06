e414347095c5d00a523121a809843e740c864195
Han ne <Han@cs470.local>
2024-12-06 01:55:23 -0800

    fix bug

--- djangoProject/insurance/admin.py
+++ djangoProject/insurance/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Patient, Billing
+
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ['id', 'first_name', 'last_name', 'date_of_birth', 'insurance_number', 'carrier_name']
+
+class BillingAdmin(admin.ModelAdmin):
+    list_display = ['invoice_number', 'invoice_date', 'service_description', 'total_billed', 'patient']
+# Register your models here
+admin.site.register(Patient)
+admin.site.register(Billing)
