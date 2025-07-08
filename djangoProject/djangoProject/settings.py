3a6190c722424792df835ba3e609d0b64a639392
jacobopatz <jacobopatz@gmail.com>
2025-07-08 18:23:19 -0500

    added wsgi capabilities

--- djangoProject/djangoProject/settings.py
+++ djangoProject/djangoProject/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = "django-insecure-pqt^2-hh9l@^bwufrq0)ej6+uoat_4qq4qf*u+safw9htwx8jg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
@@ -89,7 +89,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "djangoProject.wsgi.application"
+WSGI_APPLICATION = "djangoProject.wsgi.app"
 
 
 # Database
