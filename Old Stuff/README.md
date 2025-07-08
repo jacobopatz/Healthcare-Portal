8c04459662ec846e6aff6d64381926051d5dd3ac
jacobopatz <jacobopatz@gmail.com>
2025-07-08 17:38:42 -0500

    Deleted Unnecessary files

--- Old Stuff/README.md
+++
@@ -1,6 +0,0 @@
-# Health Track Patient Information System
- <p>created as a project for CS532, San Diego State University</p>
-
- ## Personal Development
-
- 1. Link gitHub repository to local directory
--- Old Stuff/js/landing.js
+++
@@ -1,8 +0,0 @@
-function handleButtonClick(event) {
-    window.location.href = event.target.id + '.html';
-}
-document.getElementById("index").addEventListener('click',handleButtonClick);
-// Attach the click event listener to each button
-document.querySelectorAll('.box').forEach(button => {
-    button.addEventListener('click', handleButtonClick);
-});
